f6109b9f864cfae59843a152fa3e80313e4dc435
furkanGit <furkancaliskan9801@gmail.com>
2022-11-15 09:33:54 +0100

    ciclo done

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// Data una lista della spesa, stampare sulla pagina gli elementi della lista individualmente con un ciclo for che poi commentate, e poi fate lo stesso con il while (che lasciate attivo).
+
+const array = ["Pane","Zucchine", "Pesce", "Latte"];
+
+
+for (let i = 0; i < array.length; i++) {
+
+    
+    console.log(array[i]);
+
+}
